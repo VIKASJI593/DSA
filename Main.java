fe03e358521aabcc23e7e74aa3dc074df646085a
VIKASJI593 <103217596+VIKASJI593@users.noreply.github.com>
2022-04-08 01:25:27 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+ class Solution {
+
+	public static int sum(int input[]) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+		*/
+		
+	return sumofArray(input,0) ;
+		
+	}
+    
+    public static int sumofArray(int[] arr,int startIndex) {
+		
+        if(startIndex == arr.length){
+            return 0;
+        }
+        int smallSum = sumofArray(arr,startIndex+1);
+        int mySum = smallSum+arr[startIndex];
+        return mySum;
+    }
+}
+ class Main {
+
+	static Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int n = s.nextInt();
+		int input[] = new int[n];
+		for(int i = 0; i < n; i++) {
+			input[i] = s.nextInt();
+		}
+		System.out.println(Solution.sum(input));
+	}
+}
